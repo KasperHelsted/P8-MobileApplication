f643f35476ac7224c20b1db0d02d60e1f4bdcb28
Jakob Holgersen <Jakobholgersen@gmail.com>
2018-04-12 15:36:18 +0200

    Fixed Add event accessory and add event smart device pages

--- Program/app/src/main/java/p8project/sw801/ui/event/addeventsmartdevice/AddEventSmartDevice.java
+++ Program/app/src/main/java/p8project/sw801/ui/event/addeventsmartdevice/AddEventSmartDevice.java
@@ -1,25 +1,39 @@
 package p8project.sw801.ui.event.addeventsmartdevice;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import javax.inject.Inject;
 
 import p8project.sw801.BR;
 import p8project.sw801.R;
-import p8project.sw801.databinding.ActivityAddEventBinding;
+import p8project.sw801.databinding.ActivityAddEventSmartDeviceListBinding;
 import p8project.sw801.ui.base.BaseActivity;
+import p8project.sw801.ui.event.addeventaccessory.AddEventAccessory;
 
-public class AddEventSmartDevice extends BaseActivity<ActivityAddEventBinding, AddEventSmartDeviceViewModel> implements AddEventSmartDeviceNavigator {
+public class AddEventSmartDevice extends BaseActivity<ActivityAddEventSmartDeviceListBinding, AddEventSmartDeviceViewModel> implements AddEventSmartDeviceNavigator {
     @Inject
     AddEventSmartDeviceViewModel mAddEventSmartDeviceViewModel;
+    ActivityAddEventSmartDeviceListBinding mActivityAddEventSmartDeviceListBinding;
+
+    private final ArrayList<String> arrayList = new ArrayList<>();
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //mActivityAddEventBinding = getViewDataBinding();
+        mActivityAddEventSmartDeviceListBinding = getViewDataBinding();
         mAddEventSmartDeviceViewModel.setNavigator(this);
+        setUp();
+
     }
 
     @Override
@@ -45,6 +59,41 @@ public class AddEventSmartDevice extends BaseActivity<ActivityAddEventBinding, A
     public static Intent newIntent(Context context) {
         Intent intent = new Intent(context, AddEventSmartDevice.class);
         return intent;
+    }
+
+    private void setUp(){
+
+
+        listView = mActivityAddEventSmartDeviceListBinding.listViewSmartDevice;
+        populateList();
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(AddEventSmartDevice.this, AddEventAccessory.class);
+                intent.putExtra("Name", arrayList.get(position));
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+
+    private void populateList(){
+        //TODO Change to call to viewmodel
+        arrayList.add("Hue - Smart Lights");
+        arrayList.add("Nest - Termostat");
+        ArrayAdapter adapter = new ArrayAdapter(this,R.layout.activity_add_event_list_layout, arrayList);
+        listView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data != null && requestCode == 1){
+            Bundle result = data.getBundleExtra("key");
+            Intent returnIntent = new Intent();
+            returnIntent.putExtra("key", result);
+            setResult(Activity.RESULT_OK, returnIntent);
+            finish();
+        }
     }
 
 }
