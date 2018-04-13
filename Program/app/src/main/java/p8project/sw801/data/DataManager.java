fafe9c71e233f784acc027648042bad168fc420c
Kasper Helsted <khelst13@student.aau.dk>
2018-04-13 10:57:16 +0200

    Removed ententy helpers for simpler DB

--- Program/app/src/main/java/p8project/sw801/data/DataManager.java
+++ Program/app/src/main/java/p8project/sw801/data/DataManager.java
@@ -1,30 +1,9 @@
 package p8project.sw801.data;
 
-import java.util.List;
-
-import io.reactivex.Observable;
 import p8project.sw801.data.local.db.DbHelper;
 
 /**
  * Created by Kasper Helsted on 4/4/2018.
  */
-public interface DataManager<T> extends DbHelper<T> {
-    //CoordinateHelper
-    Observable<List<T>> getAll();
-
-    Observable<List<T>> getByIds(final Integer[] ids);
-
-    Observable<T> getById(final Integer id);
-
-    Observable<Integer> getCount();
-
-    Observable<Boolean> isEmpty();
-
-    Observable<Boolean> insert(final T obj);
-
-    Observable<Boolean> insertAll(final T... obj);
-
-    Observable<Boolean> update(final T obj);
-
-    Observable<Boolean> delete(final T obj);
+public interface DataManager extends DbHelper {
 }
