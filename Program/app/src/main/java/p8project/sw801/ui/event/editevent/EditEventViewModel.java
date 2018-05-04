ee881919bb4c433b775385abdc73fe2b9d7c9047
Kasper Helsted <khelst13@student.aau.dk>
2018-05-04 09:03:44 +0200

    Edit almost done

--- Program/app/src/main/java/p8project/sw801/ui/event/editevent/EditEventViewModel.java
+++ Program/app/src/main/java/p8project/sw801/ui/event/editevent/EditEventViewModel.java
@@ -1,5 +1,6 @@
 package p8project.sw801.ui.event.editevent;
 
+import android.annotation.SuppressLint;
 import android.arch.lifecycle.MutableLiveData;
 import android.databinding.ObservableArrayList;
 import android.databinding.ObservableField;
@@ -14,8 +15,8 @@ import java.util.Date;
 import java.util.List;
 
 import p8project.sw801.data.DataManager;
-import p8project.sw801.data.local.RelationEntity.EventWithData;
 import p8project.sw801.data.local.RelationEntity.TriggerWithSmartDevice;
+import p8project.sw801.data.model.db.Event;
 import p8project.sw801.data.model.db.Trigger;
 import p8project.sw801.data.model.db.When;
 import p8project.sw801.ui.base.BaseViewModel;
@@ -23,21 +24,11 @@ import p8project.sw801.ui.custom.DayPicker;
 import p8project.sw801.utils.rx.SchedulerProvider;
 
 public class EditEventViewModel extends BaseViewModel<EditEventNavigator> {
-    protected SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
+    @SuppressLint("SimpleDateFormat")
+    private SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
 
-    public ArrayList<String> locationConditions = new ArrayList<String>() {{
-        add("No location chosen");
-        add("At Location");
-        add("Near Location");
-        add("Leaving Location");
-    }};
-    public ArrayList<String> whenConditions = new ArrayList<String>() {{
-        add("No time condition chosen");
-        add("Before this time");
-        add("At this time");
-        add("After this time");
-        add("Between these times");
-    }};
+    public ArrayList<String> locationConditions;
+    public ArrayList<String> whenConditions;
 
     public ObservableField<String> eventName = new ObservableField<>();
     public ObservableInt locationCondition = new ObservableInt(0);
@@ -48,7 +39,9 @@ public class EditEventViewModel extends BaseViewModel<EditEventNavigator> {
 
     public DayPicker dayPicker = new DayPicker();
 
-    private EventWithData eventWithData;
+    private Event event;
+    private When when;
+    private List<Trigger> triggers = new ArrayList<>();
 
     public final ObservableList<Trigger> eventTriggersObservableArrayList = new ObservableArrayList<>();
     private final MutableLiveData<List<Trigger>> eventTriggersListLiveData;
@@ -58,9 +51,24 @@ public class EditEventViewModel extends BaseViewModel<EditEventNavigator> {
         super(dataManager, schedulerProvider);
 
         eventTriggersListLiveData = new MutableLiveData<>();
+        whenConditions = new ArrayList<String>() {{
+            add("No time condition chosen");
+            add("Before this time");
+            add("At this time");
+            add("After this time");
+            add("Between these times");
+        }};
+
+        locationConditions = new ArrayList<String>() {{
+            add("No location chosen");
+            add("At Location");
+            add("Near Location");
+            add("Leaving Location");
+        }};
     }
 
     private int hourAndMinuteToInt(Integer hour, Integer minute) throws ParseException {
+        @SuppressLint("DefaultLocale")
         Date date = timeFormat.parse(
                 String.format("%d:%02d", hour, minute)
         );
@@ -70,15 +78,10 @@ public class EditEventViewModel extends BaseViewModel<EditEventNavigator> {
     }
 
     private void populate() throws IOException, ParseException {
-        eventName.set(this.eventWithData.event.getName());
-
-        When when = this.eventWithData.whens.get(0).when;
+        eventName.set(this.event.getName());
 
         dayPicker.setDays((ArrayList<Integer>) when.getListWeekDays());
 
-        List<Trigger> triggers = new ArrayList<>();
-        for (TriggerWithSmartDevice triggerWithSmartDevice : this.eventWithData.triggers)
-            triggers.add(triggerWithSmartDevice.trigger);
         addTriggersToList(triggers);
 
         if (when.getStartHour() != null && when.getStartMinute() != null) {
@@ -95,8 +98,8 @@ public class EditEventViewModel extends BaseViewModel<EditEventNavigator> {
             ));
         }
 
-        locationCondition.set(when.getLocationCondition());
         whenCondition.set(when.getTimeCondition());
+        locationCondition.set(when.getLocationCondition());
     }
 
     void loadInitialEvent(int eventId) {
@@ -112,60 +115,20 @@ public class EditEventViewModel extends BaseViewModel<EditEventNavigator> {
                         getSchedulerProvider().ui()
                 ).subscribe(
                         eventWithData -> {
-                            this.eventWithData = eventWithData;
+                            this.event = eventWithData.event;
+                            this.when = eventWithData.whens.get(0).when;
+
+                            for (TriggerWithSmartDevice triggerWithSmartDevice : eventWithData.triggers)
+                                this.triggers.add(triggerWithSmartDevice.trigger);
+
                             populate();
                         }
                 )
         );
-
-
-//        eventName.set("Test event");
-//        locationCondition.set(2);
-//        whenCondition.set(4);
-//        startTime.set(3600 * 10000);
-//        endTime.set(3600 * 10000);
-//        dayPicker.setDays(new ArrayList<Integer>() {{
-//            add(0);
-//            add(1);
-//            add(3);
-//            add(5);
-//            add(6);
-//        }});
-//        List<Trigger> test = new ArrayList<>();
-//        test.add(new Trigger() {{
-//            setNotificationText("TESTER 1");
-//        }});
-//        test.add(new Trigger() {{
-//            setNotificationText("TESTER 2");
-//        }});
-//        test.add(new Trigger() {{
-//            setNotificationText("TESTER 3");
-//        }});
-//        test.add(new Trigger() {{
-//            setNotificationText("TESTER 4");
-//        }});
-//        test.add(new Trigger() {{
-//            setNotificationText("TESTER 5");
-//        }});
-//        test.add(new Trigger() {{
-//            setNotificationText("TESTER 6");
-//        }});
-//
-//        eventTriggersListLiveData.setValue(test);
     }
 
     public void close() {
         ((EditEvent) getNavigator()).finish();
-    }
-
-    public void tester() {
-        System.out.println("locationCondition: " + locationCondition.get());
-        System.out.println("whenCondition: " + whenCondition.get());
-
-        System.out.println("days: " + dayPicker.getDays());
-
-        System.out.println("start: " + startTime.get());
-        System.out.println("end: " + endTime.get());
     }
 
     public void addTriggersToList(List<Trigger> triggerList) {
@@ -177,4 +140,13 @@ public class EditEventViewModel extends BaseViewModel<EditEventNavigator> {
         return eventTriggersListLiveData;
     }
 
+    public void tester() {
+        System.out.println("locationCondition: " + locationCondition.get());
+        System.out.println("whenCondition: " + whenCondition.get());
+
+        System.out.println("days: " + dayPicker.getDays());
+
+        System.out.println("start: " + startTime.get());
+        System.out.println("end: " + endTime.get());
+    }
 }
