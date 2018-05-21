505a70ff72d448de48ab3820271e13530a2a6661
Kasper Helsted <khelst13@student.aau.dk>
2018-05-21 11:33:00 +0200

    added acknowledgements

--- Program/app/src/main/java/p8project/sw801/di/component/AppComponent.java
+++ Program/app/src/main/java/p8project/sw801/di/component/AppComponent.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright (C) 2017 MINDORKS NEXTGEN PRIVATE LIMITED
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://mindorks.com/license/apache-v2
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
 package p8project.sw801.di.component;
 
 import android.app.Application;
@@ -13,7 +28,7 @@ import p8project.sw801.di.builder.ActivityBuilder;
 import p8project.sw801.di.module.AppModule;
 
 /**
- * Created by Kasper Helsted on 4/4/2018.
+ * Created by amitshekhar on 10/07/17.
  */
 @Singleton
 @Component(modules = {AndroidSupportInjectionModule.class, AndroidInjectionModule.class, AppModule.class, ActivityBuilder.class})
--- Program/app/src/main/java/p8project/sw801/utils/CommonUtils.java
+++ Program/app/src/main/java/p8project/sw801/utils/CommonUtils.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright (C) 2017 MINDORKS NEXTGEN PRIVATE LIMITED
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://mindorks.com/license/apache-v2
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
 package p8project.sw801.utils;
 
 import android.annotation.SuppressLint;
@@ -20,7 +35,7 @@ import java.util.Locale;
 
 import p8project.sw801.R;
 /**
- * Created by Kasper Helsted on 4/4/2018.
+ * Created by amitshekhar on 10/07/17.
  */
 public final class CommonUtils {
 
