c53a918720c70ced53561e6ea7bddc3e50590371
Kasper Helsted <khelst13@student.aau.dk>
2018-05-21 11:35:43 +0200

    small cleanup

--- Program/app/src/main/java/p8project/sw801/utils/AppConstants.java
+++ Program/app/src/main/java/p8project/sw801/utils/AppConstants.java
@@ -1,24 +1,14 @@
 package p8project.sw801.utils;
 
 
-/**
- * Created by Kasper Helsted on 4/4/2018.
- */
 public final class AppConstants {
 
     public static final String DB_NAME = "dat_database.db";
 
-    public static final long NULL_INDEX = -1L;
-
     public static final String PREF_NAME = "pre_";
-
-    public static final String STATUS_CODE_FAILED = "failed";
-
-    public static final String STATUS_CODE_SUCCESS = "success";
 
     public static final String TIMESTAMP_FORMAT = "yyyyMMdd_HHmmss";
 
     private AppConstants() {
-        // This utility class is not publicly instantiable
     }
 }
